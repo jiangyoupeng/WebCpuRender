c8beb860c57395b1f33e0514250f1dfea53e415b
zhouyoupeng <zhouyoupeng@joyy.sg>
2021-06-01 16:16:38 +0800

    整合进creator的项目中 可以同步修改项目

--- webpack-no-loader.config.js
+++ webpack-no-loader.config.js
@@ -1,18 +1,18 @@
-const path = require('path');
+const path = require("path")
 
 module.exports = {
-  entry: {
-    main: "./dist/src/main.js",
-  },
-  output: {
-    path: path.join(__dirname, 'build'),
-    filename: '[name]-bundle.js',
-  },
-  plugins: [
-    new CopyWebpackPlugin([
-      {
-        from: './html',
-      }
-    ]),
-  ],
-};
+    entry: {
+        main: "./dist/src/main.js",
+    },
+    output: {
+        path: path.join(__dirname, "build"),
+        filename: "[name]-bundle.js",
+    },
+    plugins: [
+        new CopyWebpackPlugin([
+            {
+                from: "./html",
+            },
+        ]),
+    ],
+}
